b9d0c0ec17f3773e3fe0a8cfd265efb5a194faf5
Bhushan Bangde <bhushanbangde123@gmail.com>
2022-01-10 09:49:21 +0530

    hibernate added

---
+++ src/main/java/com/te/hibernate/App.java
@@ -0,0 +1,39 @@
+package com.te.hibernate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		//for create a table
+//		Team player = new Team();
+//		player.setId(7); 
+//		player.setName("MS Dhoni");
+//		player.setSalary(1000000);
+//		player.setDesignation("Captain");
+////
+//		Team team = new Team();
+//		team.setId(18);
+//		team.setName("Virat Kohli");
+//		team.setSalary(800000);
+//		team.setDesignation("Vice Captain");
+		
+		
+		//for update the table
+//		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("player");
+//		EntityManager createEntityManager = entityManagerFactory.createEntityManager();
+//		Team team = createEntityManager.find(Team.class,18);
+//		System.out.println(team);
+		// for create a table
+//		EntityTransaction transaction = createEntityManager.getTransaction();
+//		transaction.begin();
+//		createEntityManager.persist(player);
+//		createEntityManager.persist(team);
+//		transaction.commit();
+		
+		
+	}
+}
